76a7736e1d7ea33db31b5f55d9eb0f3d3f42606b
jus1d <artfa63@gmail.com>
2024-08-26 01:05:42 +0400

    chore: Add plugs for register and login endpoints

--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -4,6 +4,8 @@ import (
 	"api/internal/app/handler"
 	"api/internal/config"
 	"api/pkg/requestid"
+	"fmt"
+	"log/slog"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,5 +29,22 @@ func (r *Router) InitRoutes() *gin.Engine {
 	router.Use(gin.Recovery())
 	router.Use(requestid.New)
 
+	api := router.Group("/api")
+	{
+		api.POST("/auth/register", r.handler.Register)
+		api.POST("/auth/login", r.handler.Login)
+	}
+
+	r.log(router.Routes())
+
 	return router
+}
+
+func (r *Router) log(routes gin.RoutesInfo) {
+	for _, route := range routes {
+		if r.config.Env == config.EnvLocal {
+			record := fmt.Sprintf("Registered handler for %s %s --> %s", route.Method, route.Path, route.Handler)
+			slog.Info(record)
+		}
+	}
 }
