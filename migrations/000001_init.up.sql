3d93b71328cee8d0afa908358b0cf93b9aa66691
jus1d <artfa63@gmail.com>
2024-08-26 16:52:50 +0400

    chore: Add migrations + postgres container

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users
+(
+    id UUID DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT now() NOT NULL
+);
