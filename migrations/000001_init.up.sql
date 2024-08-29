cc0d091f92a706c21b7776d300c51e22de0e1194
jus1d <artfa63@gmail.com>
2024-08-29 20:10:29 +0400

    fix: Update some mistakes in postgres migrations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -3,8 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users
 (
     id UUID DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(64) NOT NULL,
     created_at TIMESTAMP DEFAULT now() NOT NULL
 );
