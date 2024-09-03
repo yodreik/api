703072c688121b4f3d171ee7b263d05a7c548041
jus1d <artfa63@gmail.com>
2024-09-04 00:11:07 +0400

    chore: Update recovery email: add HTML support

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -18,14 +18,24 @@ func New(c config.Mail) *Mailer {
 
 func (m *Mailer) SendRecoveryEmail(recepient string, token string) error {
 	basepath := "http://localhost:3000"
-	return m.Send(recepient, "welnex: Password reset", fmt.Sprintf("This is your recovery link: %s/auth/password/reset?token=%s. Ignore this email, if you didn't request password reset", basepath, token))
+	body := fmt.Sprintf(`
+		<html>
+		<body>
+			<p>Click <a href="%s/auth/password/reset?token=%s">here</a> to reset your password!</p>
+            <p><b>Ignore this email if you didn't request a password reset</b></p>
+		</body>
+		</html>
+	`, basepath, token)
+
+	return m.Send(recepient, "welnex: Password reset", body)
 }
 
 func (m *Mailer) Send(recepient string, subject string, body string) error {
 	auth := smtp.PlainAuth("", m.config.Address, m.config.Password, m.config.SMTP.Address)
 
 	to := []string{recepient}
-	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\n", recepient, subject, body))
+	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\nContent-Type: text/html; charset=\"UTF-8\"\r\n\r\n%s\r\n",
+		recepient, subject, body))
 
 	return smtp.SendMail(fmt.Sprintf("%s:%s", m.config.SMTP.Address, m.config.SMTP.Port), auth, m.config.Address, to, msg)
 }
