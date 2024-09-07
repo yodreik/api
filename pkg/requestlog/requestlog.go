85350cf01c18925db52667874f6ffdab9919c4d0
jus1d <artfa63@gmail.com>
2024-09-08 00:00:22 +0400

    chore: Add more data

--- pkg/requestlog/requestlog.go
+++ pkg/requestlog/requestlog.go
@@ -2,17 +2,27 @@ package requestlog
 
 import (
 	"api/pkg/requestid"
+	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Handled initialize a middleware for logging every request
-func Handled(c *gin.Context) {
-	slog.Info("Request handled",
-		slog.String("request_id", requestid.Get(c)),
+// Completed initialize a middleware for logging every request
+func Completed(c *gin.Context) {
+	start := time.Now()
+
+	c.Next()
+
+	slog.Info("Request completed",
+		slog.String("id", requestid.Get(c)),
 		slog.String("method", c.Request.Method),
 		slog.String("uri", c.Request.URL.Path),
 		slog.String("client_ip", c.ClientIP()),
+		slog.String("duration", fmt.Sprintf("%v", time.Since(start))),
+		slog.String("host", c.Request.Host),
+		slog.String("user_agent", c.Request.UserAgent()),
+		slog.Int("status", c.Writer.Status()),
 	)
 }
