8ca2fd1a267b9eb6e7267fb9b82df62a58f0f58c
jus1d <artfa63@gmail.com>
2024-10-12 23:55:30 +0400

    chore: Add some style to confirmation email (not working on some mail clients yet)

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -2,10 +2,13 @@ package mailer
 
 import (
 	"api/internal/config"
+	"bytes"
 	"fmt"
 	"net/smtp"
+	"text/template"
 )
 
+// TODO: Move to config file
 const basepath = "http://localhost:3000"
 
 type Mailer interface {
@@ -38,24 +41,34 @@ func (s *Sender) SendRecoveryEmail(recepient string, token string) error {
 }
 
 func (s *Sender) SendConfirmationEmail(recepient string, token string) error {
-	body := fmt.Sprintf(`
-		<html>
-		<body>
-			<p>Click <a href="%s/auth/confirm?token=%s">here</a> to verify your account!</p>
-			<p>This link will be available only for 48h!</p>
-		</body>
-		</html>
-	`, basepath, token)
+	type settings struct {
+		ConfirmationLink string
+	}
 
-	return s.Send(recepient, "dreik: Account confirmation", body)
+	sets := settings{
+		ConfirmationLink: fmt.Sprintf("%s/auth/confirm?token=%s", basepath, token),
+	}
+
+	tmpl, err := template.ParseFiles("template/confirmation_email.html")
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, sets)
+	if err != nil {
+		return err
+	}
+
+	return s.Send(recepient, "dreik: Account confirmation", buf.String())
 }
 
 func (s *Sender) Send(recepient string, subject string, body string) error {
 	auth := smtp.PlainAuth("", s.config.Address, s.config.Password, s.config.SMTP.Address)
 
 	to := []string{recepient}
-	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\nContent-Type: text/html; charset=\"UTF-8\"\r\n\r\n%s\r\n",
-		recepient, subject, body))
+	msg := fmt.Sprintf("To: %s\r\nSubject: %s\r\nContent-Type: text/html; charset=\"UTF-8\"\r\nMIME-Version: 1.0\r\n\r\n%s\r\n",
+		recepient, subject, body)
 
-	return smtp.SendMail(fmt.Sprintf("%s:%s", s.config.SMTP.Address, s.config.SMTP.Port), auth, s.config.Address, to, msg)
+	return smtp.SendMail(fmt.Sprintf("%s:%s", s.config.SMTP.Address, s.config.SMTP.Port), auth, s.config.Address, to, []byte(msg))
 }
